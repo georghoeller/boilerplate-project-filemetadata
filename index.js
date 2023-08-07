7d8509c1f064f3c67c0effbbffb74a7c18a97c0f
wlnhoge <georg.hoeller@raiffeisenbank.at>
2023-08-07 19:39:28 +0200

    done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'), function (req, res) {
+
+  res.json({ 
+    name : req.file.originalname, 
+    type : req.file.mimetype, 
+    size : req.file.size
+  });
+
+});
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
